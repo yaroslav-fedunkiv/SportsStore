d653d4de0c203cd26354080589e25f623464e76c
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-20 08:58:34 +0200

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="bg-success p-2 text-center text-white">
+   This is SportsStore
+   </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
